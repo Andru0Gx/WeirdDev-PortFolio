a9402eb69f137bae362db4f614c6f6dc14eb052c
Andru0Gx <Andy20ogx@gmail.com>
2024-04-03 22:16:56 -0400

    Refactor footer component and add contacts from separate file

---
+++ src/consts/footer.ts
@@ -0,0 +1,7 @@
+export const contacts = [
+	{ tag: "Email", info: "Mail@gmail.com", svg: "envelope" },
+    { tag: "Telefono", info: "0000-0000000", svg: "phone" },
+    { tag: "Direccion", info: "Calle 1234, Ciudad, Pais", svg: "location-dot"}
+]
+
+// ToDo - Add more Social Media links
