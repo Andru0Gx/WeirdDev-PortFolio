f64a09672b49b75b1948b1be1acf73c4750ac680
Andru0Gx <Andy20ogx@gmail.com>
2024-04-07 00:52:36 -0400

    Add Typing component for animated text display

---
+++ src/consts/TypingText.ts
@@ -0,0 +1,8 @@
+export const TypingList = [
+    "Hello, I'm a web developer",
+    "Clean code, captivating design.",
+    "Crafting digital experiences.",
+    "Building your online presence.",
+    "Code that brings ideas to life.",
+    "Creating web magic daily.",
+];
