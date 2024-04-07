cc0cbc06bafe0cd47023747d6c30f00a9551610c
Andru0Gx <Andy20ogx@gmail.com>
2024-04-06 20:51:29 -0400

    Update tailwind.config.mjs with formatting changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-		colors: {
-			'bg': "var(--bg)",
-			'headline': "var(--headline)",
-			'paragraph': "var(--text)",
-			'button': "var(--button)",
-			'button_text': "var(--button-text)",
-			accent: "var(--text-accent)",
-		},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {},
+        colors: {
+            bg: "var(--bg)",
+            headline: "var(--headline)",
+            paragraph: "var(--text)",
+            button: "var(--button)",
+            button_text: "var(--button-text)",
+            accent: "var(--text-accent)",
+        },
+    },
+    plugins: [],
+};
