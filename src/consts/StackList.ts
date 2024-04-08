b00ce3093d68f85e21247986d590a748ed4c7056
Andru0Gx <Andy20ogx@gmail.com>
2024-04-08 00:54:11 -0400

    Add TechStack component to display a grid of technology logos

---
+++ src/consts/StackList.ts
@@ -0,0 +1,26 @@
+export const StackList = [
+    {
+        name: "Astro",
+        id: "astro",
+    },
+    {
+        name: "HTML5",
+        id: "html",
+    },
+    {
+        name: "CSS",
+        id: "css",
+    },
+    {
+        name: "JavaScript",
+        id: "javascript",
+    },
+    {
+        name: "TypeScript",
+        id: "typescript",
+    },
+    {
+        name: "Python",
+        id: "python",
+    },
+];
