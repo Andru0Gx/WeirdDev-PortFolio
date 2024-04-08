d2ef40983f5c029821795b58c0bd3f79f3fa46cc
Andru0Gx <Andy20ogx@gmail.com>
2024-04-08 00:52:55 -0400

    Add Pages.ts file with navigation links

---
+++ src/consts/Pages.ts
@@ -0,0 +1,15 @@
+export const pages = [
+    { name: "About", label: "about", href: "#about" },
+    {
+        name: "Projects",
+        label: "projects",
+        href: "#projects",
+        disabled: true,
+        status: "Coming Soon!",
+    },
+    {
+        name: "Contact",
+        label: "contact",
+        href: "mailto:Andres0rtiz@proton.me",
+    },
+];
