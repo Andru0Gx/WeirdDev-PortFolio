a1ec5f92aa55fb979962038bb5579817340a9dbd
Andru0Gx <Andy20ogx@gmail.com>
2024-04-12 21:19:50 -0400

    Add projects.ts file with initial project data

---
+++ src/consts/projects.ts
@@ -0,0 +1,14 @@
+export const projects = [
+    {
+        poster: "/img/projects/monagasbox.jpeg",
+        name: "MonagasBox",
+        description: "WebSite for a CrossFit Gym.",
+        techStack: [
+            { name: "Astro", id: "astro" },
+            { name: "TypeScript", id: "typescript" },
+            { name: "TailwindCSS", id: "tailwindcss" },
+        ],
+        url: "https://monagasbox.vercel.app/",
+        github: "",
+    },
+];
