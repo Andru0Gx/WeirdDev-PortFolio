887e9da09b5b74f2e4630ac3bfd1e1b860f85bc1
Andru0Gx <Andy20ogx@gmail.com>
2024-04-12 21:18:27 -0400

    Update tailwind.config.mjs to extend colors in theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,14 +2,15 @@
 export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {},
-        colors: {
-            bg: "var(--bg)",
-            headline: "var(--headline)",
-            paragraph: "var(--text)",
-            button: "var(--button)",
-            button_text: "var(--button-text)",
-            accent: "var(--text-accent)",
+        extend: {
+            colors: {
+                bg: "var(--bg)",
+                headline: "var(--headline)",
+                paragraph: "var(--text)",
+                button: "var(--button)",
+                button_text: "var(--button-text)",
+                accent: "var(--text-accent)",
+            },
         },
     },
     plugins: [],
